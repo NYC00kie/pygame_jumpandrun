cf153a12b0d47d6c55a664b09f46e0f7054b29c8
Nici Scheel <scheel.niclas@gmail.com>
2021-12-21 22:01:12 +0100

    changed standard player spirte

--- Level.py
+++ Level.py
@@ -5,7 +5,7 @@ import compress_json
 class Level():
     """docstring for Level."""
 
-    def __init__(self, levelmatrixpath, levelpicpath, spritepath="textures/pygame.png"):
+    def __init__(self, levelmatrixpath, levelpicpath, spritepath="textures/Char.png"):
         self.matrix = compress_json.load(levelmatrixpath)
         self.picpath = levelpicpath
         self.size = (len(self.matrix[0]), len(self.matrix))
