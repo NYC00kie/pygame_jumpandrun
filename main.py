9a0c22ac60cab66f2c5c8262045303c6113978dd
Nici Scheel <scheel.niclas@gmail.com>
2022-01-19 18:09:19 +0100

    main.py now launches the game as it should

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import pygame
-pygame.init()
+from level import Level
+from draw import Draw
+if __name__ == "__main__":
+    pygame.init()
+    level1 = Level(levelmatrixpath="Levels/Level1.tif.json.bz",
+                   levelpicpath="Levelpictures/Level1.tif",
+                   spritepath="textures/Char.png")
+
+    level2 = Level(levelmatrixpath="Levels/Level1.tif.json.bz",
+                   levelpicpath="Levelpictures/Level1.tif",
+                   spritepath="textures/Char.png")
+
+    level3 = Level(levelmatrixpath="Levels/Level1.tif.json.bz",
+                   levelpicpath="Levelpictures/Level1.tif",
+                   spritepath="textures/Char.png")
+
+    draw = Draw()
+
+    draw.drawmenu([level1, level2, level3], pygame)
