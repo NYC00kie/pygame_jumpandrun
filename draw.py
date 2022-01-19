f488c6bb3ba900c85ef9aef79c82a96fc777772c
Nici Scheel <scheel.niclas@gmail.com>
2022-01-19 15:44:55 +0100

    added basic menu #10, added button class

--- draw.py
+++ draw.py
@@ -1,9 +1,42 @@
-import sys
-import pygame
 import time
+import pygame
+import sys
 from level import Level
 import copy
 import numpy as np
+sys.path.append(".")
+
+
+class Button:
+    """Create a button, then blit the surface in the while loop"""
+
+    def __init__(self, text,  pos, font, bg="black", feedback=""):
+        self.x, self.y = pos
+        self.font = pygame.font.SysFont("Arial", font)
+        if feedback == "":
+            self.feedback = "text"
+        else:
+            self.feedback = feedback
+        self.change_text(text, bg)
+
+    def change_text(self, text, bg="black"):
+        """Change the text whe you click"""
+        self.text = self.font.render(text, 1, pygame.Color("White"))
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def show(self, screen):
+        screen.blit(self.surface, (self.x, self.y))
+
+    def click(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(x, y):
+                    self.change_text(self.feedback, bg="red")
 
 
 class Draw():
@@ -96,8 +129,37 @@ class Draw():
             pygame.display.flip()
             time.sleep(0.05)
 
-    def drawmenu(self, Levelist):
-        pass
+    def drawmenu(self, Levelist, pygame):
+        res = (700, 720)
+        color = (255, 255, 255)
+
+        # light shade of the button
+        color_light = (170, 170, 170)
+
+        #dark shade of the button
+        color_dark = (100, 100, 100)
+
+        screen = pygame.display.set_mode(res)
+        width = screen.get_width()
+        height = screen.get_height()
+        smallfont = pygame.font.SysFont('Corbel', 35)
+        texts = []
+        for i in range(-1, 2):
+            btn = Button(f"Level{i+1}", (width/2, height/2+50*i),
+                         font=35, bg=color_light, feedback=f"Test{i+1}")
+            texts.append(btn)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                for btn in texts:
+                    btn.click(event)
+            screen.fill((60, 25, 60))
+
+            for i in texts:
+                i.show(screen)
+
+            pygame.display.update()
 
 
 if __name__ == "__main__":
@@ -108,4 +170,4 @@ if __name__ == "__main__":
     pygame.init()
     draw = Draw()
     print(newlevel.size)
-    draw.drawlevel(newlevel, pygame)
+    draw.drawmenu(newlevel, pygame)
