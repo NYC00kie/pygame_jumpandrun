da891c9499f4613744bc2d6afba16a91bb19bad5
Nici Scheel <scheel.niclas@gmail.com>
2022-01-26 22:09:10 +0100

    needed for correct compilation with pyinstaller

--- main.py
+++ main.py
@@ -2,29 +2,41 @@ import pygame
 from level import Level
 from draw import Draw
 import sys
+import os
 sys.path.append(".")
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 if __name__ == "__main__":
     pygame.init()
-    level1 = Level(levelmatrixpath="Levels/Level1.json.bz",
-                   levelpicpath="Levelpictures/Level1.png",
-                   spritepath="textures/Player_1.png",
-                   winpicpath="textures/win_1.png",
-                   levelmusicpath="backgroundmusic/Level.mp3")
+    level1 = Level(levelmatrixpath=resource_path("Levels/Level1.json.bz"),
+                   levelpicpath=resource_path("Levelpictures/Level1.png"),
+                   spritepath=resource_path("textures/Player_1.png"),
+                   winpicpath=resource_path("textures/win_1.png"),
+                   levelmusicpath=resource_path("backgroundmusic/Level.mp3"))
 
-    level2 = Level(levelmatrixpath="Levels/Level2.json.bz",
-                   levelpicpath="Levelpictures/Level2.png",
-                   spritepath="textures/Player_2.png",
-                   winpicpath="textures/win_2.png",
-                   levelmusicpath="backgroundmusic/Level.mp3")
+    level2 = Level(levelmatrixpath=resource_path("Levels/Level2.json.bz"),
+                   levelpicpath=resource_path("Levelpictures/Level2.png"),
+                   spritepath=resource_path("textures/Player_2.png"),
+                   winpicpath=resource_path("textures/win_2.png"),
+                   levelmusicpath=resource_path("backgroundmusic/Level.mp3"))
 
-    level3 = Level(levelmatrixpath="Levels/Level3.json.bz",
-                   levelpicpath="Levelpictures/Level3.png",
-                   spritepath="textures/Player_3.png",
-                   winpicpath="textures/win_3.png",
-                   levelmusicpath="backgroundmusic/Level.mp3")
+    level3 = Level(levelmatrixpath=resource_path("Levels/Level3.json.bz"),
+                   levelpicpath=resource_path("Levelpictures/Level3.png"),
+                   spritepath=resource_path("textures/Player_3.png"),
+                   winpicpath=resource_path("textures/win_3.png"),
+                   levelmusicpath=resource_path("backgroundmusic/Level.mp3"))
 
-    draw = Draw("backgroundmusic/Homescreen.mp3")
+    draw = Draw(resource_path("backgroundmusic/Homescreen.mp3"))
 
     draw.drawmenu([level1, level2, level3], pygame)
