f7b36b7de7fc7aee008dccbc203eb7353fa831c0
Nici Scheel <scheel.niclas@gmail.com>
2022-02-02 20:38:52 +0100

    wrong option --console but needed --windowed

--- compilerscript.sh
+++ compilerscript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pyver=$(python3 --version)
 echo "Now Compiling Game with pyinstaller on Python Version $pyver"
-pyinstaller --noconfirm --onefile --console --icon "./textures/Player_1.ico" --add-data "./draw.py;." --add-data "./level.py;." --add-data "./player.py;." --add-data "./textures;textures/" --add-data "./Levelpictures;Levelpictures/" --add-data "./colorcodedmaps;colorcodedmaps/" --add-data "./backgroundmusic;backgroundmusic/"  "./main.py"
+pyinstaller --noconfirm --onefile --windowed --icon "./textures/Player_1.ico" --add-data "./draw.py;." --add-data "./level.py;." --add-data "./player.py;." --add-data "./textures;textures/" --add-data "./Levelpictures;Levelpictures/" --add-data "./colorcodedmaps;colorcodedmaps/" --add-data "./backgroundmusic;backgroundmusic/"  "./main.py"
 echo "compilation complete"
 echo "The Executable should now be in ./dist"
