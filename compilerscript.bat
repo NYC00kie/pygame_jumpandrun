c869d8e03a9a3bb41ff509626e014ab45953913d
Nici Scheel <scheel.niclas@gmail.com>
2022-02-12 13:44:36 +0100

    changed compilerscripts for Wind and Linux

--- compilerscript.bat
+++ compilerscript.bat
@@ -1,1 +1,3 @@
-pyinstaller --noconfirm --onefile --windowed --icon "./textures/Player_1.ico" --add-data "./convpictolvl.py;." --add-data "./draw.py;." --add-data "./level.py;." --add-data "./player.py;." --add-data "./textures;textures/" --add-data "./Levels;Levels/" --add-data "./Levelpictures;Levelpictures/" --add-data "./backgroundmusic;backgroundmusic/"  "./main.py"
+@echo off
+pyinstaller --noconfirm --onefile --distpath "./windows" --windowed --icon "./textures/Player_1.ico" --add-data "./convpictolvl.py;." --add-data "./versioncontrol.py;." --add-data "./draw.py;." --add-data "./level.py;." --add-data "./player.py;." --add-data "./textures;textures/" --add-data "./Levels;Levels/" --add-data "./Levelpictures;Levelpictures/" --add-data "./backgroundmusic;backgroundmusic/"  "./main.py"
+echo "File can be found in ./windows"
--- compilerscript.sh
+++ compilerscript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pyver=$(python3 --version)
 echo "Now Compiling Game with pyinstaller on Python Version $pyver"
-pyinstaller --noconfirm --onefile --windowed --icon "./textures/Player_1.ico" --add-data "./convpictolvl.py;." --add-data "./draw.py;." --add-data "./level.py;." --add-data "./player.py;." --add-data "./textures;textures/" --add-data "./Levels;Levels/" --add-data "./Levelpictures;Levelpictures/" --add-data "./backgroundmusic;backgroundmusic/"  "./main.py"
+pyinstaller --noconfirm --distpath "./linux" --onefile --windowed --icon "./textures/Player_1.ico" --add-data "./convpictolvl.py:." --add-data "./versioncontrol.py:." --add-data "./draw.py:." --add-data "./level.py:." --add-data "./player.py:." --add-data "./textures:textures/" --add-data "./Levels:Levels/" --add-data "./Levelpictures:Levelpictures/" --add-data "./backgroundmusic:backgroundmusic/"  "./main.py"
 echo "compilation complete"
-echo "The Executable should now be in ./dist"
+echo "The Executable should now be in ./linux"
